170bc3adbda2d62c86bd07316c2ce4eaf5a751ad
Danish-CodeSquare <mohammad.danish@codesquaretech.com>
2023-09-28 12:18:56 +0530

    Update vtex cart API

--- src/controllers/vtex.controller.ts
+++ src/controllers/vtex.controller.ts
@@ -307,9 +307,9 @@ export class VtexController {
     @response(200, {
       description: "Get the current cart or create a new one if it doesn't exist yet.",
     })
-    async getOrCreateCartId():Promise<any>{
+    async getOrCreateCartId(@param.header.string('token') token: string):Promise<any>{
       try{
-        const data = await this.vtexService.getOrCreateCartId()
+        const data = await this.vtexService.getOrCreateCartId(token)
         const response = await data;
         return response;
       }
