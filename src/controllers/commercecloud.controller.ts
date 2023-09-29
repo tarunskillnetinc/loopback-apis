cbaf3890cf196d7e54aedc369f5e010ac375d085
AmberDube <amber.dube@codesquaretech.com>
2023-09-29 12:03:58 +0530

    login api salesforce update and plp filter api

--- src/controllers/commercecloud.controller.ts
+++ src/controllers/commercecloud.controller.ts
@@ -1,5 +1,5 @@
-import {inject} from '@loopback/core';
-import { CommercecloudService} from '../services';
+import { inject } from "@loopback/core";
+import { CommercecloudService } from "../services";
 import {
   Request,
   RestBindings,
@@ -11,45 +11,92 @@ import {
   requestBody,
   ResponseObject,
   patch,
-} from '@loopback/rest';
-
+} from "@loopback/rest";
 
 export class CommercecloudController {
   constructor(
-    @inject('services.CommercecloudService') private sprykerService: CommercecloudService,
-    @inject(RestBindings.Http.REQUEST) private request: Request,
+    @inject("services.CommercecloudService")
+    private sprykerService: CommercecloudService,
+    @inject(RestBindings.Http.REQUEST) private request: Request
   ) {}
 
-  @get('/demo-salesforce-plp-by-category/{refine}')
-  @response(200,{
-    description: 'Get Salesforce Product List by search category',
+  @get("/demo-salesforce-plp-by-category/{refine}")
+  @response(200, {
+    description: "Get Salesforce Product List by search category",
   })
   async getSalesforceProductByCategory(
-    @param.path.string('refine') refine: any,
-    ): Promise<any>{
-    try{
-      console.log('aaff');
-      const getSalesForceProducts = await this.sprykerService.getSalesforceProductByCategory(refine);
-      console.log('aashh');
+    @param.path.string("refine") refine: any
+  ): Promise<any> {
+    try {
+      console.log("aaff");
+      const getSalesForceProducts =
+        await this.sprykerService.getSalesforceProductByCategory(refine);
+      console.log("aashh");
       return getSalesForceProducts;
-    }
-
-    catch(error){
+    } catch (error) {
       throw error;
     }
-
   }
 
-  @get('/salesforce-pdp/{productId}')
+  @get("/demo-salesforce-search-by-facets/{category}")
   @response(200, {
-    description: 'Get Salesforce product details from the external API',
+    description: "Search for products using facets",
   })
-  async salesForceProduct(@param.path.string('productId') productId: string): Promise<any> {
+  async SalesforceByFacets(
+    @param.path.string('category') category: string,
+
+    @param.query.string('color') color?: any,
+
+    @param.query.string('size') size?: any,
+    
+    @param.query.string('minprice') minprice?: any,
+
+    @param.query.string('maxprice') maxprice?: any,
+
+    @param.query.string('sortbyprice') sortbyprice?: any,
+
+    @param.query.string('sortbyname') sortbyname?: any,
+
+  ): Promise<any> {
     try {
-      const salesforceProductDetails = await this.sprykerService.getsalesForceProductById(productId);
-      console.log('salesforceProductDetails', salesforceProductDetails);
+      const data = await this.sprykerService.searchByFacets(category,color,size,minprice,maxprice,sortbyprice,sortbyname);
+      const response = await data;
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+  @get("/salesforce-pdp/{productId}")
+  @response(200, {
+    description: "Get Salesforce product details from the external API",
+  })
+  async salesForceProduct(
+    @param.path.string("productId") productId: string
+  ): Promise<any> {
+    try {
+      const salesforceProductDetails =
+        await this.sprykerService.getsalesForceProductById(productId);
+      console.log("salesforceProductDetails", salesforceProductDetails);
       return salesforceProductDetails;
     } catch (error) {
+      throw error;
+    }
+  }
+
+  @post("/salesforce-login")
+  @response(200, {
+    description: "Get Salesforce product details from the external API",
+  })
+  async salesForceLogin(
+    @requestBody() requestBody: { email: string; password: string }
+  ): Promise<any> {
+    try {
+      const salesforceloginDetails =
+        await this.sprykerService.postsalesForceLogin(requestBody);
+      console.log("salesforceloginDetails", salesforceloginDetails);
+      return salesforceloginDetails;
+    } catch (error) {
+      console.log("errorcontroller", error);
       throw error;
     }
   }
--- src/services/commercecloud.service.ts
+++ src/services/commercecloud.service.ts
@@ -1,116 +1,190 @@
-import {inject, Provider} from '@loopback/core';
-import {getService} from '@loopback/service-proxy';
-import axios from 'axios';
-import {CommercecloudDataSource} from '../datasources';
+import { inject, Provider } from "@loopback/core";
+import { getService } from "@loopback/service-proxy";
+import axios from "axios";
+import { CommercecloudDataSource } from "../datasources";
 import currencySymbol from "currency-symbol-map";
 
 export class CommercecloudService {
   constructor(
     // commercecloud must match the name property in the datasource json file
-    @inject('datasources.commercecloud')
-    protected dataSource: CommercecloudDataSource 
+    @inject("datasources.commercecloud")
+    protected dataSource: CommercecloudDataSource
   ) {}
 
   async fetchFromEndpoint(endpoint: string): Promise<any> {
     try {
-      const response = await axios.get(`${this.dataSource.settings.baseURL}/${endpoint}`);
+      const response = await axios.get(
+        `${this.dataSource.settings.baseURL}/${endpoint}`
+      );
       return response.data;
     } catch (error) {
+      console.log("error123",error)
       throw error;
     }
   }
 
+  async fetchPostFromEndpoint(
+    endpoint: string,
+    basicAuth: string
+  ): Promise<any> {
+    var data = {
+      type: "credentials",
+    };
+    var headers = {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${basicAuth}`,
+    };
+    try {
+      const response = await axios.post(
+        `${this.dataSource.settings.baseURL}/${endpoint}`,
+        data,
+        { headers: headers }
+      );
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
 
-  async getSalesforceProductByCategory(refine: any): Promise<any>{
-    const product_arr:any[] = [];
+  async getSalesforceProductByCategory(refine: any): Promise<any> {
+    const product_arr: any[] = [];
     const endpoint = `/s/Ref-VinodCSQT/dw/shop/v23_2/product_search?refine=cgid=${refine}&expand=images,prices&client_id=e0f74755-15bf-4575-8e0f-85d52b39a73b`;
+    console.log("endpoint123",endpoint)
     const response = await this.fetchFromEndpoint(endpoint);
     const value = response.refinements;
     console.log(value);
-    const valueFacets = this.getRefinementValue(value)
-    const data =  response;
-    response?.hits?.map((items:any) =>{
+    const valueFacets = this.getRefinementValue(value);
+    const data = response;
+    response?.hits?.map((items: any) => {
       product_arr.push({
-        product_id:items?.product_id,
-        sku_id:items?.product_id,
-        product_name:items?.product_name,
-        product_image:items?.image.dis_base_link,
+        product_id: items?.product_id,
+        sku_id: items?.product_id,
+        product_name: items?.product_name,
+        product_image: items?.image.dis_base_link,
         product_price: {
-          listPrice:items?.price,
-          sellingPrice:null,
-          discount:null,
-        }
-      })
-    })
-    console.log('data',product_arr);
+          listPrice: items?.price,
+          sellingPrice: null,
+          discount: null,
+        },
+      });
+    });
+    console.log("data", product_arr);
 
     // return product_arr;
-    return {"ProductData":product_arr,
-            "valueFacets": valueFacets};
+    return { ProductData: product_arr, valueFacets: valueFacets };
   }
 
   private getRefinementValue(response: any): any {
-    const value_arr:any[] = [];
-  console.log("aafreeee",response);     
+    const value_arr: any[] = [];
+    console.log("aafreeee", response);
     response?.map((item: any) => {
       value_arr.push({
-         name: item.label,
-         value: item.values.map((valueObj:any) => {
+        name: item.label,
+        value: item?.values?.map((valueObj: any) => {
           // Create a new object without the "doc_count" property
           // const { doc_count, ...newValueObj } = valueObj;
-         const newValueObj =valueObj.label;
+          const newValueObj = valueObj.label;
           return newValueObj;
         }),
+      });
     });
-  });
-  return value_arr;
+    return value_arr;
+  }
 
-} 
+  async searchByFacets(
+    category: string,
+    color: any,
+    size: any,
+    minprice: any,
+    maxprice: any,
+    sortbyprice: any,
+    sortbyname: any
+  ): Promise<any> {
+    const product_arr: any[] = [];
+    const endpoint = `/s/Ref-VinodCSQT/dw/shop/v23_2/product_search?refine=cgid=${category}&refine_1=c_refinementColor=${color==undefined?"":color}&refine_2=price=${minprice==undefined?"":(minprice..m)}&refine_3=c_size=${size==undefined?"":size}&sort=${sortbyname==undefined?"":sortbyname}&expand=images,prices&client_id=e0f74755-15bf-4575-8e0f-85d52b39a73b`;
+    console.log("endpoint1234",endpoint)
+    const response = await this.fetchFromEndpoint(endpoint);
+    const value = response.refinements;
+    console.log(value);
+    const valueFacets = this.getRefinementValue(value);
+    // const data = response;
+    console.log("responsehits123",response)
 
-async getsalesForceProductById(pid: any): Promise<any> {
-  const endpoint = `/s/Ref-VinodCSQT/dw/shop/v23_2/products/${pid}?null=null&client_id=e0f74755-15bf-4575-8e0f-85d52b39a73b&expand=images%2Cprices%2Cavailability%2Cvariations%2Cpromotions%2Cset_products`;
-  const response = await this.fetchFromEndpoint(endpoint);
-  const data = await response; // Parse JSON response
-  const variantProductsData = await this.getVariationData(data?.variants);
-  return {
-    productId: data?.id,
-    name: data?.name,
-    available: data?.inventory?.orderable,
-    description: data?.long_description,
-    skus: variantProductsData,
-  };
-
-  // return data;
-}
-private async getVariationData(response: any): Promise<any[]> {
-  const skuData: any = [];
-
-  await Promise.all(
-    response?.map(async (variantProducts: any) => {
-      const endpoint = `/s/Ref-VinodCSQT/dw/shop/v23_2/products/${variantProducts?.product_id}?null=null&client_id=e0f74755-15bf-4575-8e0f-85d52b39a73b&expand=images%2Cprices%2Cavailability%2Cvariations%2Cpromotions%2Cset_products`;
-      const response = await this.fetchFromEndpoint(endpoint);
-      skuData.push({
-        sku: response?.id,
-        skuname: response?.name,
-        dimensions: " ",
-        available: response?.inventory?.orderable,
-        availablequantity: response?.inventory?.ats,
-        listPriceFormated:
-          currencySymbol(response?.currency) + response?.price,
-        listPrice: response?.price,
-        bestPriceFormated:
-          currencySymbol(response?.currency) + response?.price_per_unit,
-        discountPercent: "",
-        bestPrice: response?.price_per_unit,
-        spotPrice: "",
-        images: {
-          image1: response?.image_groups[0]?.images[0]?.link,
+    response?.hits?.map((items: any) => {
+      product_arr.push({
+        product_id: items?.product_id,
+        sku_id: items?.product_id,
+        product_name: items?.product_name,
+        product_image: items?.image?.dis_base_link,
+        product_price: {
+          listPrice: items?.price,
+          sellingPrice: " ",
+          discount: " ",
         },
       });
-    })
+    });
+    console.log("data", product_arr);
 
-  );
-  return skuData;
-}
-  
+    // return product_arr;
+    return { ProductData: product_arr, valueFacets: valueFacets };
+  }
+
+  async getsalesForceProductById(pid: any): Promise<any> {
+    const endpoint = `/s/Ref-VinodCSQT/dw/shop/v23_2/products/${pid}?null=null&client_id=e0f74755-15bf-4575-8e0f-85d52b39a73b&expand=images%2Cprices%2Cavailability%2Cvariations%2Cpromotions%2Cset_products`;
+    const response = await this.fetchFromEndpoint(endpoint);
+    const data = await response; // Parse JSON response
+    const variantProductsData = await this.getVariationData(data?.variants);
+    return {
+      productId: data?.id,
+      name: data?.name,
+      available: data?.inventory?.orderable,
+      description: data?.long_description,
+      skus: variantProductsData,
+    };
+
+    // return data;
+  }
+  private async getVariationData(response: any): Promise<any[]> {
+    const skuData: any = [];
+
+    await Promise.all(
+      response?.map(async (variantProducts: any) => {
+        const endpoint = `/s/Ref-VinodCSQT/dw/shop/v23_2/products/${variantProducts?.product_id}?null=null&client_id=e0f74755-15bf-4575-8e0f-85d52b39a73b&expand=images%2Cprices%2Cavailability%2Cvariations%2Cpromotions%2Cset_products`;
+        const response = await this.fetchFromEndpoint(endpoint);
+        skuData.push({
+          sku: response?.id,
+          skuname: response?.name,
+          dimensions: " ",
+          available: response?.inventory?.orderable,
+          availablequantity: response?.inventory?.ats,
+          listPriceFormated:
+            currencySymbol(response?.currency) + response?.price,
+          listPrice: response?.price,
+          bestPriceFormated:
+            currencySymbol(response?.currency) + response?.price_per_unit,
+          discountPercent: "",
+          bestPrice: response?.price_per_unit,
+          spotPrice: "",
+          images: {
+            image1: response?.image_groups[0]?.images[0]?.link,
+          },
+        });
+      })
+    );
+    return skuData;
+  }
+  async postsalesForceLogin(reqBody: any): Promise<any> {
+    const endpoint = `s/Ref-VinodCSQT/dw/shop/v23_2/customers/auth?client_id=e0f74755-15bf-4575-8e0f-85d52b39a73b`;
+    var reqBody = reqBody;
+    var basicAuth = btoa(`${reqBody.email}:${reqBody.password}`);
+    console.log("reqBody", basicAuth);
+    const response = await this.fetchPostFromEndpoint(endpoint, basicAuth);
+    const data = await response; // Parse JSON response
+    console.log("responseservice123", data);
+    if (data.status == 200) {
+      return { ...data.data, bearerToken: data.headers.authorization };
+    } else {
+      return data?.data;
+    }
+  }
 }
